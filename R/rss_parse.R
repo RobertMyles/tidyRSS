1a4c5fc954ea9994f3f67a465f132e2edfa75681
Robert Myles McDonnell <robertmylesmcdonnell@gmail.com>
2022-12-28 17:22:43 +0000

    add rss duration

--- R/rss_parse.R
+++ R/rss_parse.R
@@ -46,7 +46,8 @@ rss_parse <- function(response, list, clean_tags, parse_dates) {
     item_category = map(res_entry_xml, ~ {
       xml_find_all(.x, "category") %>% map(xml_text)
     }),
-    item_comments = map(res_entry, "comments", .default = def) %>% unlist()
+    item_comments = map(res_entry, "comments", .default = def) %>% unlist(),
+    item_duration = map(res_entry, "duration", .default = def) %>% unlist()
   )
 
   # clean up
